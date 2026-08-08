f44fac85c1dce62c6ca81711aca2da832775e88f
Haseeb <ah085904@gmail.com>
2026-08-08 14:58:25 +0500

    chore:import api controller and export screenController

--- src/screenController.js
+++ src/screenController.js
@@ -1,4 +1,6 @@
-const screenController = () => {
+import { apiController } from './apiFunctions.js';
+
+export const screenController = () => {
   const main = document.querySelector('main');
   const input = document.querySelector('input[type="text"]');
   const searchBtn = document.querySelector('.search-btn');
