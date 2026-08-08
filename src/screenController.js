72a162f3790ae21835f3f4bcd29da8d3b3675de6
Haseeb <ah085904@gmail.com>
2026-08-08 14:53:43 +0500

    feat:write a render function to render weather card

--- src/screenController.js
+++ src/screenController.js
@@ -3,5 +3,52 @@ const screenController = () => {
   const input = document.querySelector('input[type="text"]');
   const searchBtn = document.querySelector('.search-btn');
 
-  function render() {}
+  function render() {
+    main.textContent = '';
+
+    const card = document.createElement('div');
+    card.classList.add('.card');
+
+    const locationDiv = document.createElement('div');
+    locationDiv.classList.add('location-div');
+
+    const locationIcon = document.createElement('img');
+    locationIcon.classList.add('location-icon');
+    locationIcon.setAttribute('alt', 'location-icon');
+
+    const location = document.createElement('p');
+    location.classList.add('location');
+
+    locationDiv.appendChild(locationIcon);
+    locationDiv.appendChild(location);
+
+    card.appendChild(locationDiv);
+
+    const weatherDiv = document.createElement('div');
+
+    weatherDiv.classList.add('weather-div');
+
+    const weatherIcon = document.createElement('img');
+    weatherIcon.setAttribute('alt', 'weather-icon');
+    weatherIcon.classList.add('weather-icon');
+
+    const temperatureDiv = document.createElement('div');
+    temperatureDiv.classList.add('temp-div');
+
+    const temperature = document.createElement('p');
+    temperature.classList.add('temp');
+
+    const feelsLikeTemp = document.createElement('p');
+    feelsLikeTemp.classList('feels-like');
+
+    temperatureDiv.appendChild(temperature);
+    temperatureDiv.appendChild(feelsLikeTemp);
+
+    weatherDiv.appendChild(weatherIcon);
+    weatherDiv.appendChild(temperatureDiv);
+
+    card.appendChild(weatherDiv);
+
+    main.appendChild(card);
+  }
 };
