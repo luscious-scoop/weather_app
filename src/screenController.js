e5af235f05041330951a6619f4c652efcfab9e1d
Haseeb <ah085904@gmail.com>
2026-08-08 14:13:35 +0500

    chore: add references to html elements in screenController

--- src/screenController.js
+++ src/screenController.js
@@ -1,1 +1,7 @@
-const screenController = () => {};
+const screenController = () => {
+  const main = document.querySelector('main');
+  const input = document.querySelector('input[type="text"]');
+  const searchBtn = document.querySelector('.search-btn');
+
+  function render() {}
+};
